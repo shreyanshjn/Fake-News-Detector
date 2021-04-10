ff3366ef62f69dbc92750432d4ff62cea07ab00d
sanditya2510 <sanditya2510@gmail.com>
2021-04-10 13:11:24 +0530

    - added whitenoise to settings

--- backend/btp/settings.py
+++ backend/btp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from whitenoise.django import DjangoWhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
